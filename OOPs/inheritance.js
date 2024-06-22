d570ad072bb9a33eaa200dbf87a41ccf3719646d
Suraj Bhalerao <154430492+SurajBhalerao27@users.noreply.github.com>
2024-06-22 19:15:45 +0530

    new file has been added call and inheritance

--- OOPs/inheritance.js
+++ OOPs/inheritance.js
@@ -1,34 +1,26 @@
-// *Old approach to having inheritance in JavaScript using object literals.
+class User {
+    constructor(username){
+        this.username = username;
+    }
 
-// User object literal
-const User = {
-    name: 'John',
-    email: 'john@gmail',
-    password: '123'
-};
-
-// Teacher object literal
-const Teacher = {
-    isTeaching: true,
-    age: 25,
-    subject: 'Javascript'
-};
-
-// Student object literal
-// Here, we are using the __proto__ property to establish inheritance from Teacher.
-// This is considered an outdated approach and is not recommended for modern JavaScript development.
-const Student = {
-    isStudying: true,
-    age: 20,
-    subject: 'Javascript',
-    study_hrs: 3,
-
-    // Dependencies on Teacher
-    __proto__: Teacher
-};
+    logMe(){
+        console.log(`Username is - ${this.username}`);
+    }
+}
 
 
-// *Modern approach to achieve inheritance in javascript
-// Modern approach to achieve inheritance in JavaScript using Object.setPrototypeOf()
-// This method sets the prototype (i.e., the internal [[Prototype]] property) of a specified object to another object or null.
-Object.setPrototypeOf(Teacher, Student);
+class Teacher extends User {
+    constructor(username, email, password){
+        super(username);
+        this.email = email;
+        this.password =  password;
+    }
+
+    addCourse(){
+        console.log(`A new course added by ${this.username}`);
+    }
+}
+
+
+const suraj = new Teacher("Demo","demo@gmail.com",123);
+console.log(suraj.addCourse());
